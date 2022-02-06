d31234b4a561359ed28d99989fc52187e063cad8
praveensingh3123 <praveensingh3123@gmail.com>
2022-02-06 11:36:52 +0530

    Create UniquePaths.java

---
+++ Walmart/UniquePaths.java
@@ -0,0 +1,23 @@
+package Walmart;
+
+public class UniquePaths {
+    public static int NumberOfPath(int a, int b)
+    {
+        int[][] dp=new int[a][b];
+        for(int i=a-1;i>=0;i--){
+            for(int j=b-1;j>=0;j--){
+                if(i==a-1 && j==b-1){
+                    dp[i][j]=1;
+                }
+                else if(i==a-1){
+                    dp[i][j]=dp[i][j+1];
+                }
+                else if(j==b-1){
+                    dp[i][j]=dp[i+1][j];
+                }
+                else dp[i][j]=dp[i][j+1]+dp[i+1][j];
+            }
+        }
+        return dp[0][0];
+    }
+}

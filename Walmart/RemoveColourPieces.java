20d9abb6bee6e18fb74d01bff9c75d4d1e12c828
praveensingh3123 <praveensingh3123@gmail.com>
2022-02-06 11:35:53 +0530

    Create RemoveColourPieces.java

---
+++ Walmart/RemoveColourPieces.java
@@ -0,0 +1,26 @@
+package Walmart;
+
+public class RemoveColourPieces {
+	 public boolean winnerOfGame(String colors) {
+	        int Aplays = 0, Bplays = 0, contA = 0, contB = 0;
+	        for (int i = 0; i < colors.length(); i++) {
+	            char c = colors.charAt(i);
+	            if (c == 'A') {
+	                contB = 0;
+	                contA++;
+	                if (contA >= 3) {
+	                    Aplays++;
+	                }
+	            }
+	            else {
+	                contA = 0;
+	                contB++;
+	                if (contB >= 3) {
+	                    Bplays++;
+	                }
+
+	            }
+	        }
+	        return Aplays > Bplays;
+	    }
+}
